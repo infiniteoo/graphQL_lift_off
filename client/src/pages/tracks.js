339357e3530542bd88296f3dab8dfbbb67b9e4ae
Troy Dorman <troydorman@gmail.com>
2021-03-23 10:12:28 -0600

    mapped our query data over our trackcards.  lookin good!

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Layout } from '../components';
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
+
 
 export const TRACKS = gql`
 
@@ -25,7 +28,26 @@ query getTracks {
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+
+
+
+  return <Layout grid>
+    {data?.tracksForHome?.map((track) => (
+
+      <TrackCard key={track.id} track={track} />
+
+    ))}
+
+
+
+
+  </Layout>;
 };
 
 export default Tracks;
