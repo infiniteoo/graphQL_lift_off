f92103bbd037649827e6adbf61d225fdf5b1324d
Troy Dorman <troydorman@gmail.com>
2021-03-23 09:11:43 -0600

    wrote our first graph ql query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
 
+export const TRACKS = gql`
+
+query getTracks {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}
+
+`
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
